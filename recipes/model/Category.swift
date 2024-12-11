00bb00f555807accb550fa34fbf799f6576e410d
Maximiliano Paris <mparis@hotmail.cl>
2024-12-10 23:56:59 -0700

    fix: delete categories with nullify

--- recipes/model/Category.swift
+++ recipes/model/Category.swift
@@ -14,7 +14,7 @@ class Category: Identifiable, Hashable {
     @Attribute(.unique) var id: UUID = UUID()
     var title: String
     var emoji: String
-    @Relationship(deleteRule: .cascade, inverse: \Recipe.categories) var recipes: [Recipe] = []
+    @Relationship(deleteRule: .nullify, inverse: \Recipe.categories) var recipes: [Recipe] = []
     
     init(title: String, emoji: String, recipes: [Recipe] = []) {
         self.title = title
